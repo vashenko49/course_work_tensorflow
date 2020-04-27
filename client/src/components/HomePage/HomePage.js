d7c4cc26e3d7d285e40b8b7127d92d6a64509719
Vashchenko Sergii <vashenko49@gmail.com>
2020-04-28 02:08:40 +0300

    test

--- client/src/components/HomePage/HomePage.js
+++ client/src/components/HomePage/HomePage.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react';
 import { Container } from '@material-ui/core';
 import Typography from '@material-ui/core/Typography';
 import StyledLink from '../styled/StyledLink';
-
 class HomePage extends Component {
   render() {
     return (
       <Container>
         <Typography variant={'h3'}>
-          On this service you can find out if a person is sick or not with tuberculosis. Not a
-          commercial version.
+          Analysis of fluorograms for tuberculosis. Not a commercial version.
         </Typography>
         <StyledLink to={'analysis'}>
           <Typography variant={'body1'}>Analysis your fluorography -></Typography>
